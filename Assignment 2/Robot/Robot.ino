99356bdaebb3cb1fc360ccb592dbefd3bdf50ba4
Wouter van der Zwan <mail@wvdzwan.nl>
2017-10-23 15:21:26 +0200

    Auto-format Arduino code

--- Assignment 2/Robot/Robot.ino
+++ Assignment 2/Robot/Robot.ino
@@ -52,25 +52,27 @@
 #define NODECOUNT 16
 
 //Matrix - Robot ID 0 to ID 15
-uint8_t matrix[NODECOUNT][NODECOUNT]={{0,1,0,1,1,1,0,1,0,0,0,0,0,1,0,1},
-                                      {1,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0},
-                                      {1,0,0,1,1,0,0,0,1,0,0,0,1,0,0,1},
-                                      {0,1,1,0,0,1,0,0,0,1,0,0,0,0,1,0},
-                                      {1,0,0,1,0,1,0,1,0,1,0,0,0,1,0,1},
-                                      {1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0},
-                                      {0,0,0,0,1,0,0,1,1,0,0,0,1,0,0,1},
-                                      {0,1,0,0,0,1,1,0,0,1,0,0,0,0,1,0},
-                                      {0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0},
-                                      {1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0},
-                                      {0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,1},
-                                      {0,1,0,0,0,1,0,0,0,1,1,0,0,1,0,0},
-                                      {0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1},
-                                      {1,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0},
-                                      {0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,1},
-                                      {0,0,1,0,0,0,1,0,0,0,0,0,0,1,1,0}};
 char ssid[] = "ES";
 char password[] = "passcode123";
 char ip[] = {192,168,43,94};
+uint8_t matrix[NODECOUNT][NODECOUNT] = {
+  {0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1},
+  {1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+  {0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+  {1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1},
+  {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+  {0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1},
+  {0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0},
+  {1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0},
+  {0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+  {0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0},
+  {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+  {0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1},
+  {0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0}
+};
 
 uint8_t Command = 0;
 long Rssi = 0;
@@ -89,104 +91,114 @@ PacketSerial packetSerial;
 void handleCommands(uint8_t src, uint8_t dst, uint8_t internal, uint8_t tcp, uint8_t fwd, uint8_t counterH, uint8_t counterL, uint8_t datalen, uint8_t command, uint8_t *data)
 {
 
-    uint8_t tempData[32] = {0};
-    data = data + 1;
-    switch(command)
-    {
-      case MOVEFORWARD : Command = MOVEFORWARD;
-                         moveForward();
-                         break;
+  uint8_t tempData[32] = {0};
+  data = data + 1;
+  switch (command)
+  {
+    case MOVEFORWARD :
+      Command = MOVEFORWARD;
+      moveForward();
+      break;
 
-      case MOVEFORWARDTIME: moveForwardForTime(*data);
-                            break;
+    case MOVEFORWARDTIME:
+      moveForwardForTime(*data);
+      break;
 
-      case MOVEBACK: Command = MOVEBACK; 
-                     moveBack();
-                     break;
-                     
-      case MOVEBACKTIME: moveBackForTime(*data);
-                         break;
-                         
-      case STOP: Command = STOP; 
-                 stopMotors();
-                 break;
-                
-      case TURNLEFT: turnLeft(*data);
-                     break;
+    case MOVEBACK:
+      Command = MOVEBACK;
+      moveBack();
+      break;
 
-      case TURNRIGHT: turnRight(*data);
-                      break;
+    case MOVEBACKTIME:
+      moveBackForTime(*data);
+      break;
 
-      case DISTANCEFRONT: distance = getDistanceFront();
-                          if(distance > 254)
-                          {
-                           distance = 254;
-                          }
-                          tempData[0] = command;
-                          tempData[1] = distance & 0xFF;
-                          tempData[2] = 0;
-                          sendPacket(dst, src, internal, tcp, ACK, counterH, counterL, 2, tempData);
-                          break;
+    case STOP:
+      Command = STOP;
+      stopMotors();
+      break;
 
-      case GETHEADING: break;   
+    case TURNLEFT:
+      turnLeft(*data);
+      break;
 
-      case GETID: nodeID = getID();
-                  tempData[0] = command;
-                  tempData[1] = nodeID;
-                  sendPacket(dst, src, internal, tcp, ACK, counterH, counterL, 2, tempData);
-                  break; 
-    }
+    case TURNRIGHT:
+      turnRight(*data);
+      break;
+
+    case DISTANCEFRONT:
+      distance = getDistanceFront();
+      if (distance > 254)
+      {
+        distance = 254;
+      }
+      tempData[0] = command;
+      tempData[1] = distance & 0xFF;
+      tempData[2] = 0;
+      sendPacket(dst, src, internal, tcp, ACK, counterH, counterL, 2, tempData);
+      break;
+
+    case GETHEADING:
+      break;
+
+    case GETID:
+      nodeID = getID();
+      tempData[0] = command;
+      tempData[1] = nodeID;
+      sendPacket(dst, src, internal, tcp, ACK, counterH, counterL, 2, tempData);
+      break;
   }
+}
 
 //Timer 1 interrupt service routine
 ISR(TIMER1_OVF_vect)
 {
- 
- long time = millis();
 
- switch(Command)
- {
+  long time = millis();
 
-  case MOVEFORWARDTIME:
-  case TURNLEFT:
-  case TURNRIGHT:
-  case MOVEBACKTIME:
-  if(((uint16_t)(millis()/1000) - tempMovementTime) >= movementTime)
-                      {
-                        stopMotors();  
-                        Command = NOCOMMAND; 
-                      }
-                     break;
- }
+  switch (Command)
+  {
 
- TCNT1 = TIMER1COUNT;
+    case MOVEFORWARDTIME:
+    case TURNLEFT:
+    case TURNRIGHT:
+    case MOVEBACKTIME:
+      if (((uint16_t)(millis() / 1000) - tempMovementTime) >= movementTime)
+      {
+        stopMotors();
+        Command = NOCOMMAND;
+      }
+      break;
+  }
+
+  TCNT1 = TIMER1COUNT;
 }
 
 void initGPIO()
 {
- pinMode(MOTOR_FRONT_LEFT_P,OUTPUT);
- pinMode(MOTOR_FRONT_LEFT_N,OUTPUT);
- pinMode(MOTOR_BACK_LEFT_P,OUTPUT);
- pinMode(MOTOR_BACK_LEFT_N,OUTPUT);
- pinMode(MOTOR_FRONT_RIGHT_P,OUTPUT);
- pinMode(MOTOR_FRONT_RIGHT_N,OUTPUT);
- pinMode(MOTOR_BACK_RIGHT_P,OUTPUT);
- pinMode(MOTOR_BACK_RIGHT_N,OUTPUT);
- pinMode(REDLED,OUTPUT);
- pinMode(WHITELED,OUTPUT);
- pinMode(YELLOWLED,OUTPUT);
- pinMode(ORANGELED,OUTPUT);
+  pinMode(MOTOR_FRONT_LEFT_P, OUTPUT);
+  pinMode(MOTOR_FRONT_LEFT_N, OUTPUT);
+  pinMode(MOTOR_BACK_LEFT_P, OUTPUT);
+  pinMode(MOTOR_BACK_LEFT_N, OUTPUT);
+  pinMode(MOTOR_FRONT_RIGHT_P, OUTPUT);
+  pinMode(MOTOR_FRONT_RIGHT_N, OUTPUT);
+  pinMode(MOTOR_BACK_RIGHT_P, OUTPUT);
+  pinMode(MOTOR_BACK_RIGHT_N, OUTPUT);
+  pinMode(REDLED, OUTPUT);
+  pinMode(WHITELED, OUTPUT);
+  pinMode(YELLOWLED, OUTPUT);
+  pinMode(ORANGELED, OUTPUT);
 
- stopMotors();
+  stopMotors();
 
- pinMode(ULTRASONIC_FRONT_TRIGGER, OUTPUT);
- pinMode(ULTRASONIC_FRONT_ECHO, INPUT);
+  pinMode(ULTRASONIC_FRONT_TRIGGER, OUTPUT);
+  pinMode(ULTRASONIC_FRONT_ECHO, INPUT);
 }
 
 
 void initTimer()
 {
-  noInterrupts();   
+  noInterrupts();
   TCCR1A = 0;
   TCCR1B = 0;
 
@@ -199,30 +211,30 @@ void initTimer()
 //Set LEDs
 void setLED(uint8_t led, bool state)
 {
-  if(state)
+  if (state)
   {
-    digitalWrite(led,HIGH);
+    digitalWrite(led, HIGH);
   }
   else
   {
-    digitalWrite(led,LOW);
+    digitalWrite(led, LOW);
   }
-  
+
 }
 
 //Get distance in cm from front ultrasonic sensor (In blocking mode)
 uint8_t getDistanceFront()
 {
- digitalWrite(ULTRASONIC_FRONT_TRIGGER, LOW); 
- delayMicroseconds(2); 
+  digitalWrite(ULTRASONIC_FRONT_TRIGGER, LOW);
+  delayMicroseconds(2);
 
- digitalWrite(ULTRASONIC_FRONT_TRIGGER, HIGH);
- delayMicroseconds(10); 
- 
- digitalWrite(ULTRASONIC_FRONT_TRIGGER, LOW);
- long duration = pulseIn(ULTRASONIC_FRONT_ECHO, HIGH);
+  digitalWrite(ULTRASONIC_FRONT_TRIGGER, HIGH);
+  delayMicroseconds(10);
 
- return duration/58.2;
+  digitalWrite(ULTRASONIC_FRONT_TRIGGER, LOW);
+  long duration = pulseIn(ULTRASONIC_FRONT_ECHO, HIGH);
+
+  return duration / 58.2;
 }
 
 //
@@ -232,36 +244,36 @@ void moveForward()
 
 {
   stopMotors();
-  digitalWrite(MOTOR_FRONT_LEFT_P,HIGH);
-  digitalWrite(MOTOR_FRONT_LEFT_N,LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_P, HIGH);
+  digitalWrite(MOTOR_FRONT_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_LEFT_P,HIGH);
-  digitalWrite(MOTOR_BACK_LEFT_N,LOW);
-  
-  digitalWrite(MOTOR_FRONT_RIGHT_P,HIGH);
-  digitalWrite(MOTOR_FRONT_RIGHT_N,LOW);
+  digitalWrite(MOTOR_BACK_LEFT_P, HIGH);
+  digitalWrite(MOTOR_BACK_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_RIGHT_P,HIGH);
-  digitalWrite(MOTOR_BACK_RIGHT_N,LOW);
+  digitalWrite(MOTOR_FRONT_RIGHT_P, HIGH);
+  digitalWrite(MOTOR_FRONT_RIGHT_N, LOW);
+
+  digitalWrite(MOTOR_BACK_RIGHT_P, HIGH);
+  digitalWrite(MOTOR_BACK_RIGHT_N, LOW);
 
 }
 
 //Stop movement
 void stopMotors()
 {
-  digitalWrite(MOTOR_FRONT_LEFT_P,LOW);
-  digitalWrite(MOTOR_FRONT_LEFT_N,LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_P, LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_LEFT_P,LOW);
-  digitalWrite(MOTOR_BACK_LEFT_N,LOW);
-  
-  digitalWrite(MOTOR_FRONT_RIGHT_P,LOW);
-  digitalWrite(MOTOR_FRONT_RIGHT_N,LOW);
+  digitalWrite(MOTOR_BACK_LEFT_P, LOW);
+  digitalWrite(MOTOR_BACK_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_RIGHT_P,LOW);
-  digitalWrite(MOTOR_BACK_RIGHT_N,LOW);
+  digitalWrite(MOTOR_FRONT_RIGHT_P, LOW);
+  digitalWrite(MOTOR_FRONT_RIGHT_N, LOW);
 
-  delay(200);  
+  digitalWrite(MOTOR_BACK_RIGHT_P, LOW);
+  digitalWrite(MOTOR_BACK_RIGHT_N, LOW);
+
+  delay(200);
 }
 
 //Move forward for specific time (in seconds)
@@ -269,7 +281,7 @@ void moveForwardForTime(uint8_t data)
 {
   moveForward();
   movementTime = data;
-  tempMovementTime = (uint16_t)(millis()/1000);
+  tempMovementTime = (uint16_t)(millis() / 1000);
   Command = MOVEFORWARDTIME;
 }
 
@@ -278,7 +290,7 @@ void moveBackForTime(uint8_t data)
 {
   moveBack();
   movementTime = data;
-  tempMovementTime = (uint16_t)(millis()/1000);
+  tempMovementTime = (uint16_t)(millis() / 1000);
   Command = MOVEBACKTIME;
 }
 
@@ -286,57 +298,56 @@ void moveBackForTime(uint8_t data)
 void moveBack()
 {
   stopMotors();
-  digitalWrite(MOTOR_FRONT_LEFT_P,LOW);
-  digitalWrite(MOTOR_FRONT_LEFT_N,HIGH);
+  digitalWrite(MOTOR_FRONT_LEFT_P, LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_N, HIGH);
 
-  digitalWrite(MOTOR_BACK_LEFT_P,LOW);
-  digitalWrite(MOTOR_BACK_LEFT_N,HIGH);
-  
-  digitalWrite(MOTOR_FRONT_RIGHT_P,LOW);
-  digitalWrite(MOTOR_FRONT_RIGHT_N,HIGH);
+  digitalWrite(MOTOR_BACK_LEFT_P, LOW);
+  digitalWrite(MOTOR_BACK_LEFT_N, HIGH);
 
-  digitalWrite(MOTOR_BACK_RIGHT_P,LOW);
-  digitalWrite(MOTOR_BACK_RIGHT_N,HIGH);
+  digitalWrite(MOTOR_FRONT_RIGHT_P, LOW);
+  digitalWrite(MOTOR_FRONT_RIGHT_N, HIGH);
+
+  digitalWrite(MOTOR_BACK_RIGHT_P, LOW);
+  digitalWrite(MOTOR_BACK_RIGHT_N, HIGH);
 }
 
 //Turn left for specific time (in seconds)
 void turnLeft(uint8_t data)
-
 {
   stopMotors();
-  digitalWrite(MOTOR_FRONT_LEFT_P,LOW);
-  digitalWrite(MOTOR_FRONT_LEFT_N,HIGH);
+  digitalWrite(MOTOR_FRONT_LEFT_P, LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_N, HIGH);
 
-  digitalWrite(MOTOR_BACK_LEFT_P,LOW);
-  digitalWrite(MOTOR_BACK_LEFT_N,HIGH);
-  
-  digitalWrite(MOTOR_FRONT_RIGHT_P,HIGH);
-  digitalWrite(MOTOR_FRONT_RIGHT_N,LOW);
+  digitalWrite(MOTOR_BACK_LEFT_P, LOW);
+  digitalWrite(MOTOR_BACK_LEFT_N, HIGH);
 
-  digitalWrite(MOTOR_BACK_RIGHT_P,HIGH);
-  digitalWrite(MOTOR_BACK_RIGHT_N,LOW);  
+  digitalWrite(MOTOR_FRONT_RIGHT_P, HIGH);
+  digitalWrite(MOTOR_FRONT_RIGHT_N, LOW);
+
+  digitalWrite(MOTOR_BACK_RIGHT_P, HIGH);
+  digitalWrite(MOTOR_BACK_RIGHT_N, LOW);
   movementTime = data;
-  tempMovementTime = (uint16_t)(millis()/1000);
+  tempMovementTime = (uint16_t)(millis() / 1000);
   Command = TURNLEFT;
 }
 
 //Turn right for specific time (in seconds)
 void turnRight(uint8_t data)
-{ 
+{
   stopMotors();
-  digitalWrite(MOTOR_FRONT_LEFT_P,HIGH);
-  digitalWrite(MOTOR_FRONT_LEFT_N,LOW);
+  digitalWrite(MOTOR_FRONT_LEFT_P, HIGH);
+  digitalWrite(MOTOR_FRONT_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_LEFT_P,HIGH);
-  digitalWrite(MOTOR_BACK_LEFT_N,LOW);
-  
-  digitalWrite(MOTOR_FRONT_RIGHT_P,LOW);
-  digitalWrite(MOTOR_FRONT_RIGHT_N,HIGH);
+  digitalWrite(MOTOR_BACK_LEFT_P, HIGH);
+  digitalWrite(MOTOR_BACK_LEFT_N, LOW);
 
-  digitalWrite(MOTOR_BACK_RIGHT_P,LOW);
-  digitalWrite(MOTOR_BACK_RIGHT_N,HIGH); 
+  digitalWrite(MOTOR_FRONT_RIGHT_P, LOW);
+  digitalWrite(MOTOR_FRONT_RIGHT_N, HIGH);
+
+  digitalWrite(MOTOR_BACK_RIGHT_P, LOW);
+  digitalWrite(MOTOR_BACK_RIGHT_N, HIGH);
   movementTime = data;
-  tempMovementTime = (uint16_t)(millis()/1000);
+  tempMovementTime = (uint16_t)(millis() / 1000);
   Command = TURNRIGHT;
 }
 
@@ -373,31 +384,31 @@ void setID(uint8_t ID)
 void sendID()
 {
   nodeID = getID();
-  sendPacket(nodeID,nodeID,INT_ID,TCP,FWD,0,0,1,&nodeID);
+  sendPacket(nodeID, nodeID, INT_ID, TCP, FWD, 0, 0, 1, &nodeID);
 }
 
 //Send connection matrix to ESP32
 void sendMatrix()
 {
   nodeID = getID();
-  sendPacket(nodeID,nodeID,INT_MATRIX,TCP,FWD,0,0,NODECOUNT,(uint8_t*)matrix[nodeID]);
+  sendPacket(nodeID, nodeID, INT_MATRIX, TCP, FWD, 0, 0, NODECOUNT, (uint8_t*)matrix[nodeID]);
 }
 
 //Send IP address of server to ESP32
 void sendIP()
 {
-  sendPacket(nodeID,nodeID,INT_IP,TCP,FWD,0,0,sizeof(ip),ip);
+  sendPacket(nodeID, nodeID, INT_IP, TCP, FWD, 0, 0, sizeof(ip), ip);
 }
 
 //Send AP SSID and password to ESP32
 void sendSSIDandPassword()
 {
-  char *ssid_pwd = (char*)calloc(strlen(ssid)+strlen(password)+2,sizeof(char));
-  strcpy(ssid_pwd,ssid);
+  char *ssid_pwd = (char*)calloc(strlen(ssid) + strlen(password) + 2, sizeof(char));
+  strcpy(ssid_pwd, ssid);
   int delimiterLoc = strlen(ssid);
   ssid_pwd[delimiterLoc] = 0xA9;
-  strcat(ssid_pwd,password);
-  sendPacket(nodeID,nodeID,INT_SSID_PWD,TCP,FWD,0,0,strlen(ssid_pwd),ssid_pwd);
+  strcat(ssid_pwd, password);
+  sendPacket(nodeID, nodeID, INT_SSID_PWD, TCP, FWD, 0, 0, strlen(ssid_pwd), ssid_pwd);
   free(ssid_pwd);
 }
 
@@ -406,11 +417,11 @@ void onPacket(const uint8_t* buffer, size_t size)
 {
   uint8_t src, dst, internal, tcp, fwd, counterH, counterL, datalen, command, *data;
   nodeID = getID();
-  if((buffer[0] != STARTBYTE))
+  if ((buffer[0] != STARTBYTE))
   {
     return;
   }
-  if(size<7)
+  if (size < 7)
   {
     return;
   }
@@ -428,9 +439,9 @@ void onPacket(const uint8_t* buffer, size_t size)
   // Checksum is not calculated. Can be implemented if necessary
 
   //Check if the command is internal, especially get RSSI from ESP32
-  if(internal == INT_RSSI)
+  if (internal == INT_RSSI)
   {
-    if(datalen != 5)
+    if (datalen != 5)
     {
       return;
     }
@@ -444,19 +455,19 @@ void onPacket(const uint8_t* buffer, size_t size)
     temp[4] = RSSI_Value;
     //sendPacket(0, 0, 0, TCP, ACK, counterH, counterL, 5, temp);
   }
-  else if(internal == INT_ID)
+  else if (internal == INT_ID)
   {
     sendID();
   }
-  else if(internal == INT_MATRIX)
+  else if (internal == INT_MATRIX)
   {
     sendMatrix();
   }
-  else if(internal == INT_SSID_PWD)
+  else if (internal == INT_SSID_PWD)
   {
     sendSSIDandPassword();
   }
-  else if(internal == INT_IP)
+  else if (internal == INT_IP)
   {
     sendIP();
   }
@@ -468,56 +479,56 @@ void onPacket(const uint8_t* buffer, size_t size)
 //For internal use only
 void sendPacket(uint8_t src, uint8_t dst, uint8_t internal, uint8_t isTCP, uint8_t isACK, uint8_t counterHigh, uint8_t counterLow, uint8_t dataLength, uint8_t *data)
 {
- uint8_t packet[PACKETSIZE] = {0};
- int index = 0;
- uint8_t checksum = 0;
- nodeID = getID();
- 
- packet[0] = STARTBYTE;
- packet[1] = (src<<4) | (dst & 0x0F);
- packet[2] = nodeID << 4;
- packet[3] = (internal << 5) | (isTCP << 4) | (isACK << 3);
- packet[4] = counterHigh;
- packet[5] = counterLow;
- packet[6] = dataLength;
- for(index=0; index<dataLength; index++)
- {
-  packet[7+index] = data[index];
- }
-// packet[7+index] = checksum;  // Checksum is not calculated. Can be implemented if necessary
- packetSerial.send(packet,7+index);
+  uint8_t packet[PACKETSIZE] = {0};
+  int index = 0;
+  uint8_t checksum = 0;
+  nodeID = getID();
+
+  packet[0] = STARTBYTE;
+  packet[1] = (src << 4) | (dst & 0x0F);
+  packet[2] = nodeID << 4;
+  packet[3] = (internal << 5) | (isTCP << 4) | (isACK << 3);
+  packet[4] = counterHigh;
+  packet[5] = counterLow;
+  packet[6] = dataLength;
+  for (index = 0; index < dataLength; index++)
+  {
+    packet[7 + index] = data[index];
+  }
+  // packet[7+index] = checksum;  // Checksum is not calculated. Can be implemented if necessary
+  packetSerial.send(packet, 7 + index);
 }
 
 //Initial setup
-void setup() 
+void setup()
 {
-Serial.begin(115200);
-//setID(4);
-nodeID = getID();
-packetSerial.setPacketHandler(&onPacket);
-packetSerial.begin(115200);
-initGPIO();
-initTimer();
-delay(2000);
-sendID();
-delay(1000);
-sendMatrix();
-delay(1000);
-sendIP();
-delay(1000);
-sendSSIDandPassword();
+  Serial.begin(115200);
+  //setID(4);
+  nodeID = getID();
+  packetSerial.setPacketHandler(&onPacket);
+  packetSerial.begin(115200);
+  initGPIO();
+  initTimer();
+  delay(2000);
+  sendID();
+  delay(1000);
+  sendMatrix();
+  delay(1000);
+  sendIP();
+  delay(1000);
+  sendSSIDandPassword();
 }
 bool once = true;
-void loop() 
+void loop()
 {
   packetSerial.update();
 }
 /** USER FUNCTION FOR AD-HOC NETWORKS COURSE. Create function and send over WiFi network
-src -> ID of robot. Send nodeID variable here (This is don't care for TCP packet)
-dst -> ID of robot to which you want to send the packet (This is don't care for TCP packet)
-isTCP -> Set with macro TCP or ADHOC depending where you want to send
-dataLength -> Length of data
-data -> Data that has to be sent. The first byte is COMMAND and subsequent bytes are arguments
+  src -> ID of robot. Send nodeID variable here (This is don't care for TCP packet)
+  dst -> ID of robot to which you want to send the packet (This is don't care for TCP packet)
+  isTCP -> Set with macro TCP or ADHOC depending where you want to send
+  dataLength -> Length of data
+  data -> Data that has to be sent. The first byte is COMMAND and subsequent bytes are arguments
 **/
 void CreatePacket(uint8_t src, uint8_t dst, uint8_t isTCP, uint8_t dataLength, uint8_t *data)
 {
@@ -525,19 +536,19 @@ void CreatePacket(uint8_t src, uint8_t dst, uint8_t isTCP, uint8_t dataLength, u
   uint8_t counterLow = PacketCounter & 0xFF;
   uint8_t counterHigh = (PacketCounter >> 8) & 0xFF;
   sendPacket(src, dst, 0x00, isTCP, FWD, counterHigh, counterLow, dataLength, data);
-  
+
 }
 
 /** USER FUNCTION FOR AD-HOC NETWORKS COURSE. This function is called when a packet is received from TCP or AD-HOC node
-//void onPacket(const uint8_t* buffer, size_t size) calls this function after parsing the packet.
+  //void onPacket(const uint8_t* buffer, size_t size) calls this function after parsing the packet.
 **/
 void OnReceive(uint8_t src, uint8_t dst, uint8_t internal, uint8_t tcp, uint8_t fwd, uint8_t counterH, uint8_t counterL, uint8_t datalen, uint8_t command, uint8_t *data)
 {
 
   //Execute commands if the command is from TCP OR if ID is equal to destination (in Ad-hoc mode)
-  if(tcp == TCP || ((tcp == ADHOC) && (nodeID == dst)))
-  { 
-      handleCommands(src, dst, internal, tcp, fwd, counterH, counterL, datalen, command, data);
+  if (tcp == TCP || ((tcp == ADHOC) && (nodeID == dst)))
+  {
+    handleCommands(src, dst, internal, tcp, fwd, counterH, counterL, datalen, command, data);
   }
 }
 
